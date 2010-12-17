fbf3ed5a44c8ae013d8fa6e1655661a948466831
Sachi <sshirasaka@54chi.com>
2010-12-16 18:15:11 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porta54::Application.config.secret_token = 'd4bdca24aab150dce0c79e3e14f83ad79d79002d38bb1aeec11e18b8d9c76574335735e8336628c2a793ae67719331b88a08464818f9266d22b48d7c14a46753'
